0eaf8a46bb4cea215957cbb94257baf79683c05c
Michael <Michael@Michael-Laptop.Speedport_W_724V_01011602_00_001>
2014-12-13 14:46:08 +0100

    reuse objects for memory efficiency

--- SoundCenterPlugin/src/com/soundcenter/soundcenter/plugin/PositionsUpdater.java
+++ SoundCenterPlugin/src/com/soundcenter/soundcenter/plugin/PositionsUpdater.java
@@ -1,8 +1,5 @@
 package com.soundcenter.soundcenter.plugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -22,61 +19,73 @@ public class PositionsUpdater implements Runnable {
 		thread = Thread.currentThread();
 		thread.setName("PositionsUpdater");
 
+		
+		long timeA;
+		long timeB;
+		int delay;
+		
+		ServerUser[] userList;
+		int i;
+		int k;
+		Player player;
+		ServerUser onlineUser;
 		while (!exit) {
-			long timeA = System.currentTimeMillis();
-
-			int i = 0;
-			List<ServerUser> userList = new ArrayList<ServerUser>(SoundCenter.userList.acceptedUsers.values());
-			for (ServerUser user : userList) {
-				Player player = user.getPlayer();
-				if (player != null && user.isInitialized()) {
-					Location loc = player.getLocation();
-					UdpPacket packet = new UdpPacket(GlobalConstants.LOCATION_PACKET_SIZE);
-					packet.setType(UdpOpcodes.INFO_LOCATION);
-					packet.setLocation(loc);
-					SoundCenter.udpServer.send(packet, user);
-
-					// update the list of listeners
-					for (int k = i; k < userList.size(); k++) {
-						ServerUser onlineUser = userList.get(k);
-						if (!onlineUser.equals(user) && (user.isSpeaking() || onlineUser.isSpeaking())) {
-							Player onlineUserPlayer = onlineUser.getPlayer();
-							if (onlineUserPlayer != null && onlineUser.isInitialized()) {
-								short distance = IntersectionDetection.playerCanHear(onlineUserPlayer,
-										user.getPlayer(), SoundCenter.config.voiceRange());
-								if (distance >= 0) {
-									byte volumePercent = (byte) (100 - ((double)distance / (double)SoundCenter.config.voiceRange())*100 );
-									if (onlineUser.isVoiceActive()) {
-										user.addListener(onlineUser, volumePercent);
+			timeA = System.currentTimeMillis();
+			
+			if (!SoundCenter.userList.acceptedUsers.isEmpty()) {
+				userList = SoundCenter.userList.acceptedUsers.values().toArray(new ServerUser[0]);
+	
+				i = 0;
+				for (ServerUser user : userList) {
+					player = user.getPlayer();
+					if (player != null && user.isInitialized()) {
+						Location loc = player.getLocation();
+						UdpPacket packet = new UdpPacket(GlobalConstants.LOCATION_PACKET_SIZE);
+						packet.setType(UdpOpcodes.INFO_LOCATION);
+						packet.setLocation(loc);
+						SoundCenter.udpServer.send(packet, user);
+	
+						// update the list of listeners
+						for (k=i; k < userList.length; k++) {
+							onlineUser = userList[k];
+							if (!onlineUser.equals(user) && (user.isSpeaking() || onlineUser.isSpeaking())) {
+								if (onlineUser.getPlayer() != null && onlineUser.isInitialized()) {
+									short distance = IntersectionDetection.playerCanHear(onlineUser.getPlayer(),
+											user.getPlayer(), SoundCenter.config.voiceRange());
+									if (distance >= 0) {
+										byte volumePercent = (byte) (100 - ((double)distance / (double)SoundCenter.config.voiceRange())*100 );
+										if (onlineUser.isVoiceActive()) {
+											user.addListener(onlineUser, volumePercent);
+										}
+										if (user.isVoiceActive()) {
+											onlineUser.addListener(user, volumePercent);
+										}
+									} else {
+										user.removeListener(onlineUser);
+										onlineUser.removeListener(user);
 									}
-									if (user.isVoiceActive()) {
-										onlineUser.addListener(user, volumePercent);
-									}
-								} else {
-									user.removeListener(onlineUser);
-									onlineUser.removeListener(user);
 								}
+							} else {
+								user.removeListener(onlineUser);
+								onlineUser.removeListener(user);
 							}
-						} else {
-							user.removeListener(onlineUser);
-							onlineUser.removeListener(user);
+						}
+	
+						if (user.isSpeaking()) {
+							// remind player that he has still activated voice chat
+							if (SoundCenter.config.voiceRemindTime() > 0
+									&& timeA - user.getSpeakingStartTime() >= SoundCenter.config.voiceRemindTime() * 1000) {
+								player.sendMessage(Messages.INFO_STILL_SPEAKING);
+								user.setSpeakingStartTime(timeA);
+							}
 						}
 					}
-
-					if (user.isSpeaking()) {
-						// remind player that he has still activated voice chat
-						if (SoundCenter.config.voiceRemindTime() > 0
-								&& timeA - user.getSpeakingStartTime() >= SoundCenter.config.voiceRemindTime() * 1000) {
-							player.sendMessage(Messages.INFO_STILL_SPEAKING);
-							user.setSpeakingStartTime(timeA);
-						}
-					}
+					i++;
 				}
-				i++;
 			}
 
-			long timeB = System.currentTimeMillis();
-			int delay = (33) - (int) (timeB - timeA);
+			timeB = System.currentTimeMillis();
+			delay = (33) - (int) (timeB - timeA);
 			
 			if (delay < 0)
 				delay = 0;
